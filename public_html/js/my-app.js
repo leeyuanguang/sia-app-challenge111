e4d2f63cb7d963610e34b21ced6bce8b60dbd6f7
Loi Liang Yang <loiliangyang@gmail.com>
2015-10-05 19:23:00 +0800

    Merge origin/master
    Conflicts:
    	public_html/inflight-beverages-detail.html

--- public_html/js/my-app.js
+++ public_html/js/my-app.js
@@ -42,6 +42,16 @@ myApp.onPageInit('inflight-beverages-detail', function (page) {
     });
 });
 
+myApp.onPageInit('inflight-amenities-detail', function (page) {
+    $$('.inflight-order-prompt').on('click', function () {
+        myApp.prompt('Please confirm your order.', function (data) {
+            // @data contains input value
+
+            myApp.alert('The attendant will be right with you.');
+        });
+    });
+});
+
 myApp.onPageInit('inflight-menu', function (page) {
     $$('.inflight-order-prompt').on('click', function () {
         
